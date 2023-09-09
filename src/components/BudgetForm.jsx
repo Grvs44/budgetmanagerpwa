ca5f640f2ea47db2a04a6c21bfc2b661441d3ada
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-09-09 15:00:35 +0100

    Create, update, and delete Budgets

--- src/components/BudgetForm.jsx
+++ src/components/BudgetForm.jsx
@@ -1,47 +1,74 @@
+import React from 'react'
 import {
   Button,
   Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   FormControlLabel,
   List,
   ListItem,
   TextField,
 } from '@mui/material'
-import React from 'react'
-import { Form } from 'react-router-dom'
 
-export default function BudgetForm({ budget = { active: true } }) {
+export default function BudgetForm({
+  budget = { active: true },
+  onClose,
+  onSubmit,
+  open = false,
+  title,
+}) {
+  const onFormSubmit = async (event) => {
+    console.log('submit')
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    await onSubmit(Object.fromEntries(formData.entries()))
+    onClose()
+  }
   return (
-    <Form method="post">
-      <List>
-        <ListItem>
-          <TextField
-            name="name"
-            defaultValue={budget.name}
-            label="Name"
-            required
-            autoComplete="false"
-          />
-        </ListItem>
-        <ListItem>
-          <TextField
-            name="description"
-            defaultValue={budget.description}
-            label="Description"
-            multiline
-          />
-        </ListItem>
-        <ListItem>
-          <FormControlLabel
-            control={<Checkbox name="active" defaultChecked={budget.active} />}
-            label="Active"
-          />
-        </ListItem>
-        <ListItem>
+    <Dialog open={open} onClose={onClose}>
+      <form onSubmit={onFormSubmit}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <List>
+            <ListItem>
+              <TextField
+                name="name"
+                defaultValue={budget.name}
+                label="Name"
+                required
+                autoComplete="false"
+              />
+            </ListItem>
+            <ListItem>
+              <TextField
+                name="description"
+                defaultValue={budget.description}
+                label="Description"
+                multiline
+              />
+            </ListItem>
+            <ListItem>
+              <FormControlLabel
+                control={
+                  <Checkbox name="active" defaultChecked={budget.active} />
+                }
+                label="Active"
+              />
+            </ListItem>
+            <ListItem></ListItem>
+          </List>
+        </DialogContent>
+        <DialogActions>
+          <Button type="button" onClick={onClose}>
+            Cancel
+          </Button>
           <Button type="submit" variant="contained">
             Save
           </Button>
-        </ListItem>
-      </List>
-    </Form>
+        </DialogActions>
+      </form>
+    </Dialog>
   )
 }
