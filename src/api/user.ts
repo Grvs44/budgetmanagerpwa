a661ff3e5b5f6214fd92ee8f2a178ccb5e893065
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2023-09-21 15:51:16 +0100

    Redirect to login page if not signed in

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,5 +1,9 @@
-import { getObject } from './api'
+import { fetchJson, getObject } from './api'
 
 export async function getUser(id: number) {
   return await getObject('user', id)
+}
+
+export async function getCurrentUser() {
+  return await fetchJson('user/me/')
 }
