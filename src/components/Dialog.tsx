01aa0b2e526a2e9de8db812bd7ceaf32cfebb5ba
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-07-02 21:34:21 +0100

    Using React Router hooks Working on list pages but needs 2 clicks to open/close

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -2,6 +2,7 @@
 import { FC, useEffect } from 'react'
 import MuiDialog from '@mui/material/Dialog'
 import type { DialogProps } from '@mui/material/Dialog'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 type OnClose = (
   event: {},
@@ -21,21 +22,28 @@ const Dialog: FC<DialogProps & { type: DialogType }> = ({
   type,
   ...props
 }) => {
+  const location = useLocation()
+  console.log('state', location.state)
+  const navigate = useNavigate()
+
   const onDialogClose: OnClose = (event, reason) => {
     if (reason == 'backClick') reason = 'escapeKeyDown'
+    else navigate(-1)
     onClose ? onClose(event, reason) : null
   }
-  const listener = (e: PopStateEvent) => {
-    if (e.state?.type == type) onDialogClose({}, 'backClick')
-  }
+
+  useEffect(() => {
+    if (props.open && location.state?.type != type)
+      onDialogClose({}, 'backClick')
+  }, [props.open, location.state])
+
   useEffect(() => {
     if (props.open) {
-      history.pushState({ type }, '')
-      addEventListener('popstate', listener)
-    } else {
-      removeEventListener('popstate', listener)
+      navigate('', { state: { type } })
+      console.log('push', type)
     }
   }, [props.open])
+
   return <MuiDialog onClose={onDialogClose} {...props} />
 }
 
