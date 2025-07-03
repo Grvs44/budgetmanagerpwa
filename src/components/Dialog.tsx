7482fc0c09059d2d40edf82ffeaa518f88895c32
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-07-03 11:24:02 +0100

    Add OpenState

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,5 +1,5 @@
 // Help from https://github.com/mui/material-ui/issues/12759#issuecomment-1674377620
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 import MuiDialog from '@mui/material/Dialog'
 import type { DialogProps } from '@mui/material/Dialog'
 import { useLocation, useNavigate } from 'react-router-dom'
@@ -17,30 +17,39 @@ export enum DialogType {
   Other,
 }
 
+enum OpenState {
+  Initial,
+  Open,
+  Done,
+}
+
 const Dialog: FC<DialogProps & { type: DialogType }> = ({
   onClose,
   type,
   ...props
 }) => {
   const location = useLocation()
-  console.log('state', location.state)
   const navigate = useNavigate()
+  const [openState, setOpenState] = useState<OpenState>(OpenState.Initial)
 
   const onDialogClose: OnClose = (event, reason) => {
+    setOpenState(OpenState.Done)
     if (reason == 'backClick') reason = 'escapeKeyDown'
     else navigate(-1)
     onClose ? onClose(event, reason) : null
+    setOpenState(OpenState.Initial)
   }
 
   useEffect(() => {
-    if (props.open && location.state?.type != type)
+    if (openState == OpenState.Open && location.state?.type != type)
       onDialogClose({}, 'backClick')
-  }, [props.open, location.state])
+  }, [location.state])
 
   useEffect(() => {
-    if (props.open) {
+    if (props.open && openState == OpenState.Initial) {
       navigate('', { state: { type } })
       console.log('push', type)
+      setOpenState(OpenState.Open)
     }
   }, [props.open])
 
